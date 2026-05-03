a1239f10edb26f63d012ada4a71e51f5cbaaabe8
Luca Heidenreich <luca_heidenreich@outlook.de>
2026-05-03 11:12:02 +0200

    Add python script

---
+++ generate_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = pd.DataFrame({
+    "country": ["Switzerland", "Germany", "France", "Italy", "Spain"],
+    "gdp_bn_usd": [800, 4000, 2800, 2100, 1400],
+    "year": 2023
+})
+
+data.to_csv("gdp_data.csv", index=False)
+print("File written: gdp_data.csv")

525802e58d48f4ae4283ef05e96b9858f9ea8e4a
PhilipHeidenreich <philip_heidenreich@web.de>
2026-05-03 11:19:49 +0200

    Script added population mn

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 data = pd.DataFrame({
     "country": ["Switzerland", "Germany", "France", "Italy", "Spain"],
     "gdp_bn_usd": [800, 4000, 2800, 2100, 1400],
+    "population_mn": [8.7,84.4, 68.2,59.0,47.4],
     "year": 2023
 })
 
